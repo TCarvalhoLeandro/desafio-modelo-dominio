6ab0658a13af4d5947c451db785c296a4234100a
Leandro <trairicarvalho@gmail.com>
2026-04-14 10:15:11 -0300

    first: cria projeto

---
+++ src/main/java/com/leandro/desafio_modelo_dominio/DesafioModeloDominioApplication.java
@@ -0,0 +1,13 @@
+package com.leandro.desafio_modelo_dominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandro/desafio_modelo_dominio/DesafioModeloDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.desafio_modelo_dominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
